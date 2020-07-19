5eb81b3b8d7cb786f3ceefd35977b5fcecc2f739
Jansen Fink <jansenfink@charter.net>
2020-07-19 01:24:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,36 +1,61 @@
-def second_supply_for_fourth_of_july(holiday_supplies)
-  holiday_supplies[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(holiday_hash)
+   {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
-second_supply_for_fourth_of_july(holiday_supplies)
-
-def add_supply_to_winter_holidays(holiday_hash, item)
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations << item
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter].each do |holiday, decorations|
+    decorations << supply
   end
 end
 
-
-add_supply_to_winter_holidays(holiday_supplies, "Baloons")
-
-
-
-
 def add_supply_to_memorial_day(holiday_hash, supply)
-
-  holiday_hash[:spring][:memorial_day].push(supply)
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
-add_supply_to_memorial_day(holiday_supplies, "baloons")
-
-
-
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-
+  
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
-add_new_holiday_with_supplies(holiday_supplies, :fall, :succas, ["lulav", "esrog", "schach"])
+
+def all_winter_holiday_supplies(holiday_hash)
+  array = holiday_hash[:winter].map do |holiday, supply|
+  supply
+end
+array.flatten
+end
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
+end
+end
+
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten.compact
+end
 
 def add_new_season_and_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season] = {holiday_name => supply_array} # the new echelon being created is to the left of the equals sign.
